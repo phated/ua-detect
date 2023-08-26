7fb2b2bec10a212d14f03b3925a27b7473d5aee7
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-08-25 19:23:20 -0700

    feat: Scaffold protobuf server following tonic guide

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use validate::validate_server::{Validate, ValidateServer};
+use validate::{ValidateUserAgentRequest, ValidateUserAgentResponse};
+
+pub mod validate {
+    tonic::include_proto!("validate");
+}
+
+#[derive(Debug, Default)]
+pub struct ValidatorService;
+
+#[tonic::async_trait]
+impl Validate for ValidatorService {
+    async fn validate_user_agent(
+        &self,
+        request: Request<ValidateUserAgentRequest>,
+    ) -> Result<Response<ValidateUserAgentResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = ValidateUserAgentResponse { is_valid: false };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:9001".parse()?;
+    let validator = ValidatorService::default();
+
+    Server::builder()
+        .add_service(ValidateServer::new(validator))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
